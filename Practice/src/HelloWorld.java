f7205e0af44e9fa77e4daa9f98b1ce657b84386a
user <user@DESKTOP-UQBCK9V>
2021-06-02 12:43:57 +0800

    First commit

---
+++ Practice/src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hi, Mirae!");
+		System.out.println("I'm learning JAVA!!!");
+		System.out.println("Hi!!");
+	}
+
+}
